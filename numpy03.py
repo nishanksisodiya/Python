4d2310be766b9119cd169852f5cc4a3900778929
nony14 <nishanksisodiya@gmail.com>
2018-08-14 14:55:06 +0530

    14-08-2018

---
+++ numpy03.py
@@ -0,0 +1,16 @@
+#Python array concatenation and splitting
+import time
+import numpy as np
+import numpy.matrixlib as ml
+def main():
+    # n=int(input())
+    # m=int(input())
+    # t1=time.time()
+    # arr1=np.array([[ int(input()) for i in range(n)]for j in range(m)])
+    # t2=time.time()
+    # print(arr1)
+    # print(t1)
+    # print(t2)
+    l=list(map(int,input().split(",")))
+    print(l)
+main()
